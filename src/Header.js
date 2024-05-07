c3e34dd75f13002a50b248489e901842459589ef
Gethin Davies <110056268+GethinDavies1990@users.noreply.github.com>
2024-05-07 20:59:33 +0100

    feat: add avatar to header componnet

--- src/Header.js
+++ src/Header.js
@@ -3,6 +3,10 @@ import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
 import HomeIcon from "@mui/icons-material/Home";
 import GroupIcon from "@mui/icons-material/Group";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import FaceIcon from "@mui/icons-material/Face";
 import HeaderOption from "./HeaderOption";
 
 function Header() {
@@ -21,6 +25,13 @@ function Header() {
                   <div className="header__right">
                         <HeaderOption Icon={HomeIcon} title="Home" />
                         <HeaderOption Icon={GroupIcon} title="My Network" />
+                        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+                        <HeaderOption Icon={ChatIcon} title="Messaging" />
+                        <HeaderOption
+                              Icon={NotificationsIcon}
+                              title="Notifications"
+                        />
+                        <HeaderOption avatar="src/elon.jpeg" title="me" />
                   </div>
             </div>
       );
