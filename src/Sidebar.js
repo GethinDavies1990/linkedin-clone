26b2b813999bf547ed119c296c1c6878d00a96cf
Gethin Davies <110056268+GethinDavies1990@users.noreply.github.com>
2024-05-07 21:43:24 +0100

    style: bottom panel

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,7 +6,10 @@ function Sidebar() {
       return (
             <div className="sidebar">
                   <div className="sidebar__top">
-                        <img src="" alt="" />
+                        <img
+                              src="https://images.unsplash.com/photo-1554034483-04fda0d3507b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                              alt=""
+                        />
                         <Avatar className="sidebar__avatar" />
                         <h2>Gethin Davies</h2>
                         <h4>gethindavies1990@gmail.com</h4>
@@ -14,11 +17,11 @@ function Sidebar() {
                   <div className="sidebar__stats">
                         <div className="sidebar__stat">
                               <p>Who viewed you</p>
-                              <p className="sidebar_statNumber">2,534</p>
+                              <p className="sidebar__statNumber">2,534</p>
                         </div>
                         <div className="sidebar__stat">
                               <p>Views on my Post</p>
-                              <p className="sidebar_statNumber">234</p>
+                              <p className="sidebar__statNumber">234</p>
                         </div>
                   </div>
                   <div className="sidebar__bottom">
