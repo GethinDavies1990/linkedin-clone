39094d0e47938738cd2f1594714ee4e7cae3ef8b
Gethin Davies <110056268+GethinDavies1990@users.noreply.github.com>
2024-05-11 14:16:33 +0100

    style: post widget section

--- src/Posts.js
+++ src/Posts.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Posts.css";
+import InputOption from "./InputOption";
 import { Avatar } from "@mui/material";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import AddCommentIcon from "@mui/icons-material/AddComment";
+import ShareIcon from "@mui/icons-material/Share";
+import SendIcon from "@mui/icons-material/Send";
 
 function Posts({ name, description, message, photoUrl }) {
       return (
@@ -8,12 +13,34 @@ function Posts({ name, description, message, photoUrl }) {
                   <div className="post__header">
                         <Avatar />
                         <div className="post__info">
-                              <h2>Gethin Davies</h2>
-                              <p>Description</p>
+                              <h2>{name}</h2>
+                              <p>{description}</p>
                         </div>
                   </div>
                   <div className="post__body">
-                        <p>Message</p>
+                        <p>{message}</p>
+                  </div>
+                  <div className="post__buttons">
+                        <InputOption
+                              Icon={ThumbUpIcon}
+                              title="Like"
+                              color="gray"
+                        />
+                        <InputOption
+                              Icon={AddCommentIcon}
+                              title="Comment"
+                              color="gray"
+                        />
+                        <InputOption
+                              Icon={ShareIcon}
+                              title="Share"
+                              color="gray"
+                        />
+                        <InputOption
+                              Icon={SendIcon}
+                              title="Send"
+                              color="gray"
+                        />
                   </div>
             </div>
       );
