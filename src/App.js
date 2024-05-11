674c6381988eb16b0b79638b90b8c873f04edb04
Gethin Davies <110056268+GethinDavies1990@users.noreply.github.com>
2024-05-11 12:54:22 +0100

    style: feed section - top conatiner input

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React from "react";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import Feed from "./Feed";
 import "./App.css";
 
 function App() {
       return (
             <div className="app">
-                  {/* {Header} */}
                   <Header />
-
                   {/* App body */}
                   <div className="app__body">
                         <Sidebar />
+                        <Feed />
+                        {/*  Feed */}
+                        {/* Widgets */}
                   </div>
-                  {/*  Feed */}
-                  <Feed />
-                  {/* Widgets */}
             </div>
       );
 }
