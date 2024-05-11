e9b7d00ed2524eef741f06327742edaafaa14e19
Gethin Davies <110056268+GethinDavies1990@users.noreply.github.com>
2024-05-11 13:49:08 +0100

    feat: add inputoption component

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Feed.css";
+import InputOption from "./InputOption";
 import CreateIcon from "@mui/icons-material/Create";
+import ImageIcon from "@mui/icons-material/Image";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import EventIcon from "@mui/icons-material/Event";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 
 function Feed() {
       return (
@@ -12,6 +17,28 @@ function Feed() {
                                     <input type="text" />
                                     <button type="submit">Send</button>
                               </form>
+                        </div>
+                        <div className="feed__inputOptions">
+                              <InputOption
+                                    title="Photo"
+                                    Icon={ImageIcon}
+                                    color="#70B5F9"
+                              />
+                              <InputOption
+                                    title="Video"
+                                    Icon={SubscriptionsIcon}
+                                    color="#E7A33E"
+                              />
+                              <InputOption
+                                    title="Event"
+                                    Icon={EventIcon}
+                                    color="#C0CBCD"
+                              />
+                              <InputOption
+                                    title="Write Article"
+                                    Icon={CalendarMonthIcon}
+                                    color="#7FC15E"
+                              />
                         </div>
                   </div>
             </div>
