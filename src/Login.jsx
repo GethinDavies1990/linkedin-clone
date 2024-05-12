57e8de81f7f346554a91354e28e6a544e39bbd57
Gethin Davies <110056268+GethinDavies1990@users.noreply.github.com>
2024-05-12 13:35:16 +0100

    add state values to login functionality

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
+import { auth } from "./firebase";
 
 function Login() {
-  const register = () => {};
-  const loginToApp = () => {};
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('';)
+
+  const register = () => {
+  };
+
+  const loginToApp = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className='login'>
       <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/LinkedIn_Logo.svg/1280px-LinkedIn_Logo.svg.png' />
       <form>
-        <input placeholder='Full Name (required if registering)' type='text' />
+        <input placeholder='Full Name (required if registering)' type='text' value={name} onChange={(e) => setName(e.target.value)} />
         <input placeholder='Profile pic URL (optional)' type='text' />
-        <input type='email' placeholder='Email' />
-        <input type='password' placeholder='Password' />
+        <input type='email' placeholder='Email' value={email} oncChange={(e) => setEmail(e.target.value)} />
+        <input type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
         <button type='submit' onClick={loginToApp}>
           Sign In
         </button>
